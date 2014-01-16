d275520144cdc66bc1414806eee9b4d17317e405
Robin Beck <robin.a.beck>
2014-01-16 10:45:42 -0700

    added pair programming partner

---
+++ js2.js
@@ -0,0 +1,8 @@
+var anyword = prompt("enter any word!") ;
+alert("The word you entered is '" + anyword +"'.") ;
+alert("The number of characters in your word is " + anyword.length) ;
+alert("The third character of the word you entered is '" + anyword.substring(2,3) +"'.") ;
+alert("The word you entered in lowercase is '" + anyword.toLowerCase() +"'.") ;
+alert("The word you entered in uppercase is '" + anyword.toUpperCase() +"'.") ;
+alert("TThe lorem ipsum of gooboy is " + anyword +"!!!") ;
+alert("The second to forth characters of your word are '" + anyword.substring(1,4) +"'.") ;
